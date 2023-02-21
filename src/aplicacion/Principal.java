dc114b3e26c1f261626eab125246701e5ef46f3d
Álvaro <ainiguezdislaa@gmail.com>
2023-02-21 16:21:24 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,12 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Dime un número:");
+        int n=sc.nextInt();
+        System.out.println("La suma de los primeros  números pares hasta el  "+n+" es " + dominio.ej2_7_lambda.sumapares(n));
+    }
 }
--- src/dominio/ej2_7_lambda.java
+++ src/dominio/ej2_7_lambda.java
@@ -1,2 +1,9 @@
-package dominio;public class ej2_7_lambda {
+package dominio;
+import java.util.stream.Stream;
+public class ej2_7_lambda {
+    public static int sumapares(int n){
+        return Stream.iterate(0,i->i+1).limit(n+1).filter(i->i%2==0).reduce(0,(a,b)->a+b);
+    }
 }
+
+
